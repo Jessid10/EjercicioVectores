c554ee044cde6d4f3122454469ad0dede79f3835
JESID <JESID@Jessid_Dariio>
2014-09-02 13:30:22 -0500

    Ejercicio de Vectores de Validacion y Verificacion

---
+++ src/vectores/Vectores.java
@@ -0,0 +1,57 @@
+
+
+package vectores;
+
+
+
+import javax.swing.JOptionPane;
+public class Vectores {
+
+    
+    public static void main(String[] args) {
+       
+        int tamano;
+        int dato;
+        int m=0;
+        int n=0;
+        int mostrar1=0;
+        int mostrar2=0;
+        
+        tamano=Integer.parseInt(JOptionPane.showInputDialog("Ingrese el tamaño de los vectores"));
+        vectorOperaciones v1=new vectorOperaciones(tamano);
+        vectorOperaciones v2=new vectorOperaciones(tamano);
+        
+       while(n<tamano || m<tamano)
+        {
+         dato=Integer.parseInt(JOptionPane.showInputDialog("Ingrese un Dato: "));
+         
+         if(dato<20 && n<tamano)
+         {
+           v1.llenarVector(n, dato);
+           n++;
+         }
+         else if(dato>=20 && m<tamano)
+         {
+          v2.llenarVector(m, dato);
+           m++;
+         }
+         
+         
+         if(n==tamano && mostrar1==0)
+         {
+          JOptionPane.showMessageDialog(null,v1.mostrar());
+          mostrar1++;
+          JOptionPane.showMessageDialog(null,"El Vector 1 Esta lleno");
+         }
+         else if(m==tamano && mostrar2==0)
+         {
+           JOptionPane.showMessageDialog(null,v2.mostrar());
+           mostrar2++;
+           JOptionPane.showMessageDialog(null,"El Vector 2 Esta lleno");
+           break;
+         }
+     
+    }
+    
+}
+}
---
+++ src/vectores/vectorOperaciones.java
@@ -0,0 +1,35 @@
+
+
+package vectores;
+
+
+public class vectorOperaciones {
+    
+    private int[] vector;
+    
+    public vectorOperaciones(int tamano)
+    {
+      vector=new int[tamano];
+    }
+    
+    public void llenarVector(int pos,int dato)
+    {
+      vector[pos]=dato;
+    }
+    
+    public String mostrar()
+    {
+      String imprimir="";
+      
+      for(int i=0;i<vector.length;i++)
+      {
+       imprimir+="["+vector[i]+"]";
+      }
+      
+      return imprimir;
+    }
+           
+            
+    
+
+}
